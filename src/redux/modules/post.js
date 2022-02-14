a4accc0b352662057b13b812dfee59d702d5fc67
hovinee <hobinskyyy@naver.com>
2022-02-14 15:05:55 +0900

    getpost

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -6,7 +6,7 @@ import instance from "../../shared/Request";
 const GET_POST = "GET_POST";
 const ADD_POST = "ADD_POST"
 
-const getPost = createAction(GET_POST, (ll) => ({ ll }));
+const getPost = createAction(GET_POST, (post_list) => ({ post_list }));
 const addPost = createAction(ADD_POST, (post) => ({ post }));
 
 const initialState = {
@@ -22,34 +22,51 @@ const initialState = {
 
 // middleware
 const addPostFB = (title, location, comment, preview) => {
-    return function(dispatch, getState, {history}) {
-     
-        //    const _post = {
-    //         title: title,
-    //         location: location,
-    //         comment: comment,
-    //         image_url: preview,
-    //     }
-        
-    //     let post = {..._post}
-    //     // 만들어둔 instance에 보낼 요청 타입과 주소로 요청합니다. 
-    //     instance.post('/api/addpost/save', // 미리 약속한 주소
-    //         {title: title, location: location, comment: comment, image_url: preview}, // 서버가 필요로 하는 데이터를 넘겨주고,
-    //     ).then((res) => {
-    //         console.log(res)
-    //         dispatch(addPost(post))
-    //     })
-    //     .catch((error) => {
-    //         console.log(error);
-    //     });
+    return function (dispatch, getState, { history }) {
+
+        const _post = {
+            title: title,
+            location: location,
+            comment: comment,
+            image_url: preview,
+        }
+
+        let post = { ..._post }
+        // 만들어둔 instance에 보낼 요청 타입과 주소로 요청합니다. 
+        instance.post('/api/addpost/save', // 미리 약속한 주소
+            { title: title, location: location, comment: comment, image_url: preview }, // 서버가 필요로 하는 데이터를 넘겨주고,
+        ).then((res) => {
+            console.log(res)
+            dispatch(addPost(post))
+            history.push('/')
+        })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 }
 
 const getPostFB = () => {
-    return function(dispatch, getState, {history}) {
-        const ll = "ll"
-        console.log(ll)
-        dispatch(getPost(ll))
+    return function (dispatch, getState, { history }) {
+        instance.get('/api/main', {
+
+        }).then(function (response) {
+            console.log(response.data.response)
+            const postDB = response.data.response
+            console.log(postDB)
+            const post_list = []
+            postDB.forEach((v, i) => {
+                let list = {
+                    title: v.title,
+                    user_nick: v.user_nick, 
+                    createDate: v.createDate, post_id: v._id
+                }
+                post_list.push(list)
+            })
+            console.log(post_list)
+            dispatch(getPost(post_list))
+        })
+            .catch(function (error) { console.log(error) })
     }
 }
 
@@ -57,12 +74,12 @@ export default handleActions(
     {
         [GET_POST]: (state, action) => produce(state, (draft) => {
             console.log(action.payload)
-            draft.list=action.payload.post_list
+            draft.list = action.payload.post_list
         }),
 
         [ADD_POST]: (state, action) => produce(state, (draft) => {
-            console.log(action.payload)
-            // draft.list.unshift(action.payload.post);
+
+            draft.list.unshift(action.payload.post);
         })
     },
     initialState
