5efea9ea9197a5e8f30e27f6027cce9bf9736cbd
hovinee <hobinskyyy@naver.com>
2022-02-14 23:47:00 +0900

    이미지수정중

--- src/shared/Upload.js
+++ src/shared/Upload.js
@@ -10,36 +10,20 @@ const Upload = (props) => {
     const fileInput = React.useRef();
 
     const selectFile = (e) => {
-        // e.target은 input이죠!
-        // input이 가진 files 객체를 살펴봅시다.
-        // console.log(e.target.files);
-        // 선택한 파일이 어떻게 저장되어 있나 봅시다.
-        // console.log(e.target.files[0]);
-
-        // ref로도 확인해봅시다. :)
-        // console.log(fileInput.current.files[0]);
-
+      console.log(e.target.files[0])
+      if ( /\.(jpe?g|png|gif)$/i.test(e.name) ) {
         const reader = new FileReader();
+       
         const file = e.target.files[0];
-        
-        // 파일 내용을 읽어옵니다.
         reader.readAsDataURL(file);
-    
-        // 읽기가 끝나면 발생하는 이벤트 핸들러예요! :)
+
         reader.onloadend = () => {
-          // reader.result는 파일의 컨텐츠(내용물)입니다!
+ 
           dispatch(imageActions.setPreview(reader.result));
         };
+      }
+      
     };
-
-    // const uploadFB = () => {
-    //     if (!fileInput.current || fileInput.current.files.length === 0) {
-    //       window.alert("파일을 선택해주세요!");
-    //       return;
-    //     }
-    
-    //     dispatch(imageActions.uploadImageFB(fileInput.current.files[0]));
-    //   };
 
       return (
         <React.Fragment>
@@ -49,7 +33,6 @@ const Upload = (props) => {
             onChange={selectFile}
             disabled={uploading}
           />
-          {/* <Button _onClick={uploadFB}>업로드하기</Button> */}
         </React.Fragment>
       );
 };
